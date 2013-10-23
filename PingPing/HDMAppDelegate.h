44d76372327d86e1d93bc7ffcd2691765a6dcc49
Lukas Spieß <lukas.spiess@gmail.com>
2013-10-23 23:05:32 +0200

    Initial Commit

---
+++ PingPing/HDMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HDMAppDelegate.h
+//  PingPing
+//
+//  Created by Lukas Spieß on 23.10.13.
+//  Copyright (c) 2013 HdM Stuttgart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HDMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PingPing/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PingPing
+//
+//  Created by Lukas Spieß on 23.10.13.
+//  Copyright (c) 2013 HdM Stuttgart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HDMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HDMAppDelegate class]));
+    }
+}
