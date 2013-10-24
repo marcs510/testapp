985c8ef9bb52172b7a4beebeca41c76241cfcc2c
Lukas Spieß <lukas.spiess@gmail.com>
2013-10-24 17:33:02 +0200

    rudimentäres Interface

---
+++ PingPing/HDMServerAddViewController.h
@@ -0,0 +1,13 @@
+//
+//  HDMServerAddViewController.h
+//  PingPing
+//
+//  Created by Lukas Spieß on 24.10.13.
+//  Copyright (c) 2013 Marc Stauffer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HDMServerAddViewController : UIViewController
+
+@end
---
+++ PingPing/HDMServerDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  HDMServerDetailViewController.h
+//  PingPing
+//
+//  Created by Lukas Spieß on 24.10.13.
+//  Copyright (c) 2013 Marc Stauffer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HDMServerDetailViewController : UIViewController
+
+@end
