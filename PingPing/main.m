9cddd95413c20056836c1c7a8167ed4d681067bb
Marc Stauffer <marcs510@aol.com>
2013-10-24 14:40:08 +0200

    Initial commit

---
+++ PingPing/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PingPing
+//
+//  Created by Marc Stauffer on 24.10.13.
+//  Copyright (c) 2013 Marc Stauffer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HDMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HDMAppDelegate class]));
+    }
+}
---
+++ PingPingTests/PingPingTests.m
@@ -0,0 +1,32 @@
+//
+//  PingPingTests.m
+//  PingPingTests
+//
+//  Created by Marc Stauffer on 24.10.13.
+//  Copyright (c) 2013 Marc Stauffer. All rights reserved.
+//
+
+#import "PingPingTests.h"
+
+@implementation PingPingTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PingPingTests");
+}
+
+@end
